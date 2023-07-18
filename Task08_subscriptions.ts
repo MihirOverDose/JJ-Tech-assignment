f6288c7925e3295265326ea5c5949a88e3842982
Mihir Sidhdhapura <mihir.s@squareboat.com>
2023-07-18 12:20:46 +0530

    COMPLETED: assignment 8

--- Task08_subscriptions.ts
+++ Task08_subscriptions.ts
@@ -8,19 +8,58 @@ import { Prefix, readConfig } from "./utils/config";
 // messages - Array of MessageSubscription - Optional - The messages to be subscribed to.
 // changes
 
-const subscriptionDraft: SubscriptionDraft = {
-    key: "subscriptionSample",
-    destination: {
-        type: "GoogleCloudPubSub",
-        projectId: "ct-support",
-        topic: "training-subscription-sample"
-    },
-    messages: [{
-        resourceTypeId: "order",
-        types: ["OrderCreated"]
-    }]
-}
+//response
+// {
+//     "id": "072fe6b1-e62a-4e61-8938-d7fbd7c0f8b9",
+//     "version": 1,
+//     "versionModifiedAt": "2023-07-18T06:23:42.491Z",
+//     "createdAt": "2023-07-18T06:23:42.491Z",
+//     "lastModifiedAt": "2023-07-18T06:23:42.491Z",
+//     "lastModifiedBy": {
+//         "clientId": "lFzMFIKmRmCkpiR6NbsG35pj",
+//         "isPlatformClient": false
+//     },
+//     "createdBy": {
+//         "clientId": "lFzMFIKmRmCkpiR6NbsG35pj",
+//         "isPlatformClient": false
+//     },
+//     "destination": {
+//         "type": "GoogleCloudPubSub",
+//         "projectId": "ct-support",
+//         "topic": "training-subscription-sample"
+//     },
+//     "messages": [
+//         {
+//             "resourceTypeId": "order",
+//             "types": [
+//                 "OrderCreated"
+//             ]
+//         }
+//     ],
+//     "changes": [],
+//     "format": {
+//         "type": "Platform"
+//     },
+//     "status": "Healthy",
+//     "key": "subscriptionSample"
+// }
 
+// C O N G R A T U L A T I O N S : HTTP Status 201
+
+const subscriptionDraft: SubscriptionDraft = {
+  key: "subscriptionSample",
+  destination: {
+    type: "GoogleCloudPubSub",
+    projectId: "ct-support",
+    topic: "training-subscription-sample",
+  },
+  messages: [
+    {
+      resourceTypeId: "order",
+      types: ["OrderCreated"],
+    },
+  ],
+};
 
 // const subscriptionDraft: SubscriptionDraft = {
 //     key: "subscriptionSample",
@@ -36,10 +75,9 @@ const subscriptionDraft: SubscriptionDraft = {
 //     }]
 // }
 
-
 apiRoot
-    .subscriptions()
-    .post({ body: subscriptionDraft })
-    .execute()
-    .then(log)
-    .catch(log);
+  .subscriptions()
+  .post({ body: subscriptionDraft })
+  .execute()
+  .then(log)
+  .catch(log);
